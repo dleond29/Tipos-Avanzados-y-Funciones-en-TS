a7a8de2b0c11df1a432124d20a8f603ed3169d28
Diego Leon <diegoleon.77@hotmail.com>
2023-01-03 18:03:12 -0500

    Solve issues in main.ts

--- src/app/main.ts
+++ src/app/main.ts
@@ -4,11 +4,12 @@ addProduct({
   id: '1',
   title: 'p1',
   createAt: new Date(),
+  updateAt: new Date(),
   stock: 100,
   category: {
     id: '2',
     name: 'c1',
     createAt: new Date(),
     updateAt: new Date(),
-  }
+  },
 })
